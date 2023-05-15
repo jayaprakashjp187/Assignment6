9588af2a2da38582948b8c9d68c682ab0ae84572
jayaprakash <jayaprakashjp187@gmail.com>
2023-05-15 16:35:20 +0530

    perfect number

---
+++ Assignment6/PerfectNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6
+{
+     class PerfectNumber
+    {
+        public static void CheckingPerfectNumber(int num)
+        {
+            
+            int sum = 0;
+            for (int i = 1; i < num; i++)
+            {
+                if (num%i == 0)
+                {
+                    sum += i;
+                }
+            }
+            
+            Console.WriteLine(sum);
+            if(num==sum)
+            {
+                Console.WriteLine(num + "is a perfect number");
+            }
+            else
+            {
+                Console.WriteLine(num + "is not a perfect number");
+            }
+        }
+    }
+}
--- Assignment6/Program.cs
+++ Assignment6/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1.FibonacciSeries");
+            Console.WriteLine("1.FibonacciSeries\n2.PerfectNumberProgram");
             Console.WriteLine("please choose what program u want");
             int choose =Convert.ToInt32(Console.ReadLine());
             switch(choose)
@@ -14,6 +14,12 @@
                     Console.WriteLine("please enter how many series u want");
                     int series = Convert.ToInt32(Console.ReadLine());
                     FibonacciSeries.CheckingFibonacciSeries(series);
+                    break;
+                case 2:
+                    Console.WriteLine("welcome to the perfect number program");
+                    Console.WriteLine("please enter any number for checking perfect number or not");
+                    int perfectNumber=Convert.ToInt32(Console.ReadLine());
+                    PerfectNumber.CheckingPerfectNumber(perfectNumber);
                     break;
             }
         }
