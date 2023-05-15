2214f020bab8324f2909649cb1e6f515c1a30248
jayaprakash <jayaprakashjp187@gmail.com>
2023-05-15 16:06:19 +0530

    FibonacciSeries

---
+++ Assignment6/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Assignment6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.FibonacciSeries");
+            Console.WriteLine("please choose what program u want");
+            int choose =Convert.ToInt32(Console.ReadLine());
+            switch(choose)
+            {
+                case 1:
+                    Console.WriteLine("welcome to the fibonacci series program");
+                    Console.WriteLine("please enter how many series u want");
+                    int series = Convert.ToInt32(Console.ReadLine());
+                    FibonacciSeries.CheckingFibonacciSeries(series);
+                    break;
+            }
+        }
+    }
+}
