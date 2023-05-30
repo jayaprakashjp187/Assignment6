41a4bd4c2fdab9d32b8298bebabd2cee2eebcd45
jayaprakash <jayaprakashjp187@gmail.com>
2023-05-30 08:37:22 +0530

    Monthlypayment

--- Assignment6/Temparature.cs
+++ Assignment6/Temparature.cs
@@ -8,16 +8,24 @@ namespace Assignment6
 {
     internal class Temparature
     {
+
         public static void CelsiusToFahrenheit(double temp)
         {
             double F = (temp * 9 / 5) + 32;
             Console.WriteLine(temp + "°C in Fahrenheit is: " + F + "°F");
         }
 
+         public static void CelsiusToFahrenheit(double temp)
+          {
+            double F = (temp * 9 / 5) + 32;
+            Console.WriteLine(temp + "°C in Fahrenheit is: "+F+ "°F");
+
+          }
+
         public static void FahrenheitToCelsius(double temp)
-        {
+          {
             double C = (temp - 32) * 5 / 9;
             Console.WriteLine(temp + "°F in Celsius is: " + C + "°C");
-        }
+          }
     }
 }
